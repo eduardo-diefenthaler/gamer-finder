8b68d5a1404ff14fd86469516c149116a70347d5
eduardo-diefenthaler <eduardo.diefenthaler@gmail.com>
2022-11-04 22:44:44 -0300

    wip

---
+++ src/main/java/com/eduardo/gamerfinder/GamerfinderApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.gamerfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/gamerfinder/GamerfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.gamerfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
